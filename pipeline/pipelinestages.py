da4ebf0f9755ca273866fb4f7efc9a0e4263c781
kuehnleh <kuehnleh@ethz.ch>
2023-11-16 16:04:34 +0100

    adapted pipelinestages.py

--- pipeline/pipelinestages.py
+++ pipeline/pipelinestages.py
@@ -1,5 +1,5 @@
 ############
-# Define the pipeline stages beginning from stage 2
+# Define the pipeline stages beginning from stage 2 to stage 3
 ############
 
 import os, sys, glob
@@ -18,6 +18,7 @@ from jwst import datamodels # JWST datamodels
 from jwst.associations import asn_from_list as afl # Tools for creating association files
 from jwst.associations.lib.rules_level2_base import DMSLevel2bBase # Definition of a Lvl2 association file
 from jwst.associations.lib.rules_level3_base import DMS_Level3_Base # Definition of a Lvl3 association file
+from astropy.io import fits
 
 
 def runspec2(filename, output):
@@ -82,3 +83,86 @@ def runspec3(filename, out_dir):
     spec3.extract_1d.subtract_background = False  # , take ring around as background and subtract, only do this the first time
     spec3.extract_1d.ifu_rscale = 1  # set number of FWHMs fro radius
     spec3(filename)
+
+
+def checkdata()
+    data_str = [d for d in glob.glob(output_cubes + "*_x1d.fits")]
+    data_str = sorted(data_str)
+    # data = fits.getdata('/Users/helenakuehnle/Dateien/PhD/Analysis/WISE_J0855/pipelined/stage3/cubes/Level3_ch2-short_x1d.fits')
+    data_all = []
+
+    for i in range(len(data_str)):
+        data = data_str[i]
+        print(data)
+        data = fits.getdata(data)
+        dataz = np.array(list(zip(*data)))
+        data_all.append(dataz)
+
+    data_str3d = [d for d in glob.glob(output_cubes + "*_s3d.fits")]
+    data_str3d = sorted(data_str3d)
+    # data = fits.getdata('/Users/helenakuehnle/Dateien/PhD/Analysis/WISE_J0855/pipelined/stage3/cubes/Level3_ch2-short_x1d.fits')
+    data_all3d = []
+
+    for i in range(len(data_str3d)):
+        data = data_str3d[i]
+        print(data)
+        data = fits.getdata(data)
+        dataz = np.array(list(zip(*data)))
+        data_all3d.append(dataz)
+
+    # plot the model
+    plt.figure(figsize=(12, 4))
+    for i in range(len(data_str)):
+        plt.plot(data_all[i][0], data_all[i][1])
+
+    plt.plot(data_nirspec_prism[0], data_nirspec_prism[1], color='k')
+    plt.plot(data_nirspec_395m[0], data_nirspec_395m[1], color='r')
+    plt.ylim([-0.0001, 0.002])
+    plt.xlim([3, 24])
+    # plt.plot(x,y,linewidth=0.5,color='k')
+    plt.xlabel('wavelength [mum]')
+    plt.ylabel('Flux [Jy]')
+
+if __name__ == '__main__':
+    input_dir = '/Users/helenakuehnle/Dateien/PhD/Analysis/WISE_J0855/'
+
+    # Point to where you want the output science results to go
+    output_dir = os.path.join(input_dir, 'pipelined/')
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    det1_dir = os.path.join(output_dir, 'stage1/')  # Detector1 pipeline outputs will go here
+    spec2_dir = os.path.join(output_dir, 'stage2/')  # Spec2 pipeline outputs will go here
+    spec3_dir = os.path.join(output_dir, 'stage3/')  # Spec3 pipeline outputs will go here
+
+    # We need to check that the desired output directories exist, and if not create them
+    if not os.path.exists(det1_dir):
+        os.makedirs(det1_dir)
+    if not os.path.exists(spec2_dir):
+        os.makedirs(spec2_dir)
+    if not os.path.exists(spec3_dir):
+        os.makedirs(spec3_dir)
+
+    # reordering of files
+    files = [f for f in glob.glob(input_dir + "*/*rate.fits")]
+    files = sorted(files)
+    print(f"Found {len(files)} files to process")
+
+    # run stage 2
+    for f in files:
+        runspec2(f, spec2_dir + f.split("/")[-2] + "/")
+
+    # Find and sort all of the input files
+    output_cubes = spec3_dir + "cubes_man1b/"
+    if not os.path.exists(output_cubes):
+        os.mkdir(output_cubes)
+
+    # run stage 3
+    for folder in ["obs5_MRS_MEDIUM"]:  # ["obs5_MRS_SHORT", "obs5_MRS_MEDIUM", "obs5_MRS_LONG"]:
+        sstring = spec2_dir + folder + '/*calnodsub.fits'
+        calfiles = np.array(sorted(glob.glob(sstring)))
+        print('Found ' + str(len(calfiles)) + f' input files to process for folder {folder}')
+        # Make an association file that includes all of the different exposures
+        asnfile = os.path.join(spec2_dir + folder, 'l3asn.json')
+        writel3asn(calfiles, asnfile, 'Level3')
+        runspec3(asnfile, output_cubes)
